218669f33d19dfcfbf08df149c230bd25b826b47
@hexlet/cli <support@hexlet.io>
2023-08-02 16:02:19 +0300

    submit java-collections-ru/tests_02.08.23_15.50.50

---
+++ java-collections-ru/tests_02.08.23_15.50.50/src/test/java/exercise/AppTest.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testTake() {
+        // BEGIN
+        List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> numbersRez = App.take(numbers1, 2);
+        Assertions.assertEquals(numbersRez.size(), 2);
+        Assertions.assertEquals(numbersRez.get(0), 1);
+        // END
+    }
+}
