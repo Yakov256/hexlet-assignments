f35219f0a7d1bdc3c29febc513d6f43a3b5210a7
@hexlet/cli <support@hexlet.io>
2023-08-02 16:20:04 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/main/java/exercise/App.java
+++ java-collections-ru/tests/src/main/java/exercise/App.java
@@ -1,8 +1,22 @@
 package exercise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class App {
+
+    public static void main(String[] args) {
+        List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> numbersRez = App.take(numbers1, 2);
+        System.out.println(numbersRez.size());
+        System.out.println(numbersRez.get(0));
+        System.out.println(numbersRez);
+        
+        List<Integer> numbers2 = new ArrayList<>(Arrays.asList(7, 3, 10));
+        System.out.println(App.take(numbers2, 8)); // => [7, 3, 10]
+
+    }
     public static List<Integer> take(List<Integer> elements, int count) {
         String implementation = System.getenv("IMPLEMENTATION") != null
             ? System.getenv("IMPLEMENTATION")
--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,8 +1,12 @@
 package exercise;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
@@ -10,10 +14,17 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
+        // Some change. Nothing changed. Skip committing.
         List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        List<Integer> numbersRez = App.take(numbers1, 2);
-        Assertions.assertEquals(numbersRez.size(), 2);
-        Assertions.assertEquals(numbersRez.get(0), 1);
+        List<Integer> numbersRez1 = App.take(numbers1, 2);
+        Assertions.assertEquals(numbersRez1.size(), 2);
+        Assertions.assertEquals(numbersRez1.get(0), 1);
+        Assertions.assertEquals(numbersRez1.toString(), "[1, 2]");
+
+        List<Integer> numbers2 = new ArrayList<>(Arrays.asList(7, 3, 10));
+        List<Integer> numbersRez2 = App.take(numbers2, 8);
+        Assertions.assertEquals(numbersRez2.get(2), 10);
+        Assertions.assertEquals(numbersRez2.toString(), "[7, 3, 10]");
         // END
     }
 }
