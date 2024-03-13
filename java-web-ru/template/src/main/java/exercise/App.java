22931a484bf8d980c6c7d2bfb7a9cc333af1bc79
@hexlet/cli <support@hexlet.io>
2024-03-13 22:16:13 +0300

    submit java-web-ru/template

--- java-web-ru/template/src/main/java/exercise/App.java
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -2,8 +2,6 @@ package exercise;
 
 import io.javalin.Javalin;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import io.javalin.http.NotFoundResponse;
 import exercise.model.User;
@@ -23,44 +21,21 @@ public final class App {
         });
 
         // BEGIN
-
-        /*app.get("/users", ctx -> {
-            var courses = USERS;
-            //var page = new UsersPage(USERS);
-            List<String> lst = new ArrayList<>();
-            lst.add("123");
-            lst.add("asd");
-            HashMap<String, String > hm = new HashMap<>();
-            hm.put("1", "qwe");
-            hm.put("2", "asd");
-            ctx.render("users/index.jte", hm);
-        });*/
-
         app.get("/users", ctx -> {
             var page = new UsersPage(USERS);
             ctx.render("users/index.jte", Collections.singletonMap("page", page));
         });
 
-
         app.get("/users/{id}", ctx -> {
             var id = ctx.pathParam("id");
             Long idL = Long.parseLong(id);
             var user = USERS.stream().filter(u -> u.getId() == idL).findFirst().orElse(null);
-            if(user == null) {
+            if (user == null) {
                 throw new NotFoundResponse("User not found");
             }
             var userPage = new UserPage(user);
             ctx.render("users/show.jte", Collections.singletonMap("page", userPage));
         });
-
-        /*app.get("/users", ctx -> {
-            ctx.render("users/index.jte");
-        });*/
-
-        /*app.get("/", ctx -> {
-            ctx.render("index.jte");
-        });*/
-
         // END
 
         app.get("/", ctx -> {
